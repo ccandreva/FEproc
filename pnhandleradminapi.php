e9ff0f6c0c444ad0f42b6bec9b29d9f5c77c7ff1
Christopher X. Candreva <chris@westnet.com>
2011-06-30 20:30:21 +0000

    Update for DBUtil compliance.

--- pnhandleradminapi.php
+++ pnhandleradminapi.php
@@ -1,30 +1,34 @@
 <?php
-// ----------------------------------------------------------------------
-// FEproc - Mail template backend module for FormExpress for
-// POST-NUKE Content Management System
-// Copyright (C) 2002 by Jason Judge
-// ----------------------------------------------------------------------
-// Based on:
-// PHP-NUKE Web Portal System - http://phpnuke.org/
-// ----------------------------------------------------------------------
-// LICENSE
-//
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License (GPL)
-// as published by the Free Software Foundation; either version 2
-// of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WIthOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// To read the license please visit http://www.gnu.org/copyleft/gpl.html
-// ----------------------------------------------------------------------
-// Original Author of file: Jason Judge.
-// Based on template by Jim MacDonald.
-// Current Maintainer of file: Klavs Klavsen <kl-feproc@vsen.dk>
-// ----------------------------------------------------------------------
+/**
+ * FEproc - Mail template backend module for FormExpress for 
+ *   Zikula Content Management System
+ * 
+ * @copyrightt (C) 2002 by Jason Judge, 2011 Chris Candreva
+ * @Version $Id: tables.php 84 2011-05-27 18:19:28Z ccandreva $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @package FEproc
+ *
+ *
+ * LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License (GPL)
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WIthOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * To read the license please visit http://www.gnu.org/copyleft/gpl.html
+ * ----------------------------------------------------------------------
+ * Original Author of file: Jason Judge.
+ * Based on template by Jim MacDonald.
+ * Current Maintainer of file: Chris Candreva <chris@westnet.com>
+ * ----------------------------------------------------------------------
+ * 
+ */
 
 
 /**
@@ -37,78 +41,18 @@
  */
 function feproc_handleradminapi_create($args)
 {
-    // Get arguments from argument array.
-    extract($args);
-
-    // Argument check - make sure that all required arguments are present,
-    // if not then set an appropriate error message and return
-    if (!isset($name) ||
-        !isset($description) ||
-        !isset($type) ||
-        !isset($version) ||
-        !isset($modulename) ||
-        !isset($apiname) ||
-        !isset($apifunc) ||
-        !isset($attributes))
-    {
-        pnSessionSetVar('errormsg', _FXMODARGSERROR);
-        return false;
-    }
-
     // Early security check.
-    if (!pnSecAuthAction(0, 'FEproc::', "::", ACCESS_ADD)) {
-        pnSessionSetVar('errormsg', _FXNOAUTH);
-        return false;
+   if (!SecurityUtil::checkPermission ('FEproc::', '::', ACCESS_ADD)) {
+        return LogUtil::registerPermissionError();
     }
 
-    $sattributes = serialize($attributes);
+    // Until all calls are refactored, '$args' is the object itself
+    $obj = &$args;
+    $obj['attributes'] = serialize($obj['attributes']);
+    DBUtil::insertObject($obj, 'feproc_handlers');
 
-    // Get datbase setup.
-    list($dbconn) = pnDBGetConn();
-    $pntable = pnDBGetTables();
-
-    $handlertable = $pntable['feproc_handlers'];
-    $handlercolumn = &$pntable['feproc_handlers_column'];
-
-    // Get next ID in table - this is required prior to any insert that
-    // uses a unique ID, and ensures that the ID generation is carried
-    // out in a database-portable fashion
-    $nextId = $dbconn->GenId($handlertable);
-
-    // Add item.
-    $sql = "INSERT INTO $handlertable (
-              $handlercolumn[id],
-              $handlercolumn[name],
-              $handlercolumn[description],
-              $handlercolumn[type],
-              $handlercolumn[version],
-              $handlercolumn[modulename],
-              $handlercolumn[apiname],
-              $handlercolumn[apifunc],
-              $handlercolumn[attributes])
-            VALUES (
-              $nextId,
-              '" . pnVarPrepForStore($name) . "',
-              '" . pnVarPrepForStore($description) . "',
-              '" . pnVarPrepForStore($type) . "',
-              '" . pnVarPrepForStore($version) . "',
-              '" . pnVarPrepForStore($modulename) . "',
-              '" . pnVarPrepForStore($apiname) . "',
-              '" . pnVarPrepForStore($apifunc) . "',
-              '" . pnVarPrepForStore($sattributes) . "')";
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        pnSessionSetVar('errormsg', _FXCREATEFAILED . ' ' . $sql);
-        return false;
-    }
-
-    // Get the ID of the item that we inserted.  It is possible, although
-    // very unlikely, that this is different from $nextId as obtained
-    // above, but it is better to be safe than sorry in this situation
-    $hid = $dbconn->PO_Insert_ID($handlertable, $handlercolumn['id']);
+    // Get the ID of the item that we inserted.
+    $hid = $obj['id'];
 
     // Let any hooks know that we have created a new item.
     // TODO: this is not a standard item
@@ -127,67 +71,20 @@ function feproc_handleradminapi_create($args)
  */
 function feproc_handleradminapi_delete($args)
 {
+    // Early security check.
+   if (!SecurityUtil::checkPermission ('FEproc::', '::', ACCESS_DELETE)) {
+        return LogUtil::registerPermissionError();
+    }
     // Get arguments from argument array.
     extract($args);
 
-    // Argument check - make sure that all required arguments are present,
-    // if not then set an appropriate error message and return.
     if (!isset($hid)) {
-        pnSessionSetVar('errormsg', _FXMODARGSERROR);
-        return false;
+        return LogUtil::registerError( __('Missing handler ID'), 500);
     }
 
-    // Early security check.
-    if (!pnSecAuthAction(0, 'FEproc::', "$hid::", ACCESS_DELETE)) {
-        pnSessionSetVar('errormsg', _FXNOAUTH);
-        return false;
-    }
+    deleteObjectByID ('feproc_handlers', $hid);
 
-    if (!pnModAPILoad('feproc', 'handleruser')) {
-        pnSessionSetVar('errormsg', _FXMODLOADFAILED);
-        pnRedirect(pnModURL('feproc', 'admin', 'view'));
-        return true;
-    }
-
-    // The API function is called.  This takes the item ID which
-    // we obtained from the input and gets us the information on the
-    // appropriate item.  If the item does not exist we post an appropriate
-    // message and return
-    $item = pnModAPIFunc('feproc',
-            'handleruser',
-            'gethandler',
-            array('hid' => $hid));
-
-    if ($item == false) {
-        pnSessionSetVar('errormsg', _FXNOSUCHTEMPLATE);
-        return false;
-    }
-
-    // Get datbase setup.
-    list($dbconn) = pnDBGetConn();
-    $pntable = pnDBGetTables();
-
-    $handlertable = $pntable['feproc_handlers'];
-    $handlercolumn = &$pntable['feproc_handlers_column'];
-
-    // Delete the item - the formatting here is not mandatory, but it does
-    // make the SQL statement relatively easy to read.  Also, separating
-    // out the sql statement from the Execute() command allows for simpler
-    // debug operation if it is ever needed
-    $sql = "DELETE FROM $handlertable
-            WHERE $handlercolumn[id] = " . pnVarPrepForStore($hid);
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        pnSessionSetVar('errormsg', _FXDELETEFAILED);
-        return false;
-    }
-
-    // Let any hooks know that we have deleted an item.  As this is a
-    // delete hook we're not passing any extra info
-    // TODO: distinguish between different types of item.
+    // Let any hooks know that we have deleted a handerl
     pnModCallHooks('item', 'delete', $hid, 'hid');
 
     // Let the calling process know that we have finished successfully
@@ -204,84 +101,16 @@ function feproc_handleradminapi_delete($args)
  */
 function feproc_handleradminapi_update($args)
 {
-    // Get arguments from argument array.
-    extract($args);
-
-    // Argument check - make sure that all required arguments are present,
-    // if not then set an appropriate error message and return
-    if (!isset($hid) ||
-        !isset($name) ||
-        !isset($description) ||
-        !isset($type) ||
-        !isset($version) ||
-        !isset($modulename) ||
-        !isset($apiname) ||
-        !isset($apifunc) ||
-        !isset($attributes))
-    {
-        pnSessionSetVar('errormsg', _FXMODARGSERROR);
-        return false;
-    }
-
-    if (!pnModAPILoad('feproc', 'handleruser'))
-    {
-      pnSessionSetVar('errormsg', _FXMODLOADFAILED);
-      return false;
-    }
-
-    // The API function is called.  This takes the item ID which
-    // we obtained from the input and gets us the information on the
-    // appropriate item.  If the item does not exist we post an appropriate
-    // message and return
-    $item = pnModAPIFunc('feproc',
-            'handleruser',
-            'gethandler',
-            array('hid' => $hid));
-
-    if ($item == false)
-    {
-        pnSessionSetVar('errormsg', _FXNOSUCHTEMPLATE);
-        return false;
-    }
-
     // Early security check.
-
-    if (!pnSecAuthAction(0, 'FEproc::', "$hid::", ACCESS_EDIT))
-    {
-        pnSessionSetVar('errormsg', _FXNOAUTH);
-        return false;
+   if (!SecurityUtil::checkPermission ('FEproc::', '::', ACCESS_EDIT)) {
+        return LogUtil::registerPermissionError();
     }
 
-    // The attributes will be an array.
-    $sattributes = serialize($attributes);
-    
-    // Get datbase setup.
-    list($dbconn) = pnDBGetConn();
-    $pntable = pnDBGetTables();
-
-    $handlertable = $pntable['feproc_handlers'];
-    $handlercolumn = &$pntable['feproc_handlers_column'];
-
-    // Update the item.
-    $sql = "UPDATE $handlertable
-            SET $handlercolumn[name] = '" . pnVarPrepForStore($name) . "',
-                $handlercolumn[description] = '" . pnVarPrepForStore($description) . "',
-                $handlercolumn[type] = '" . pnVarPrepForStore($type) . "',
-                $handlercolumn[version] = '" . pnVarPrepForStore($version) . "',
-                $handlercolumn[modulename] = '" . pnVarPrepForStore($modulename) . "',
-                $handlercolumn[apiname] = '" . pnVarPrepForStore($apiname) . "',
-                $handlercolumn[apifunc] = '" . pnVarPrepForStore($apifunc) . "',
-                $handlercolumn[attributes] = '" . pnVarPrepForStore($sattributes) . "'
-            WHERE $handlercolumn[id] = " . pnVarPrepForStore($hid);
-
-    $dbconn->Execute($sql);
-
-    // Check for an error with the database code, and if so set an
-    // appropriate error message and return
-    if ($dbconn->ErrorNo() != 0) {
-        pnSessionSetVar('errormsg', _FXUPDATEFAILED . ' ' . $sql);
-        return false;
-    }
+    // Until all calls are refactored, '$args' is the object itself
+    $obj = &$args;
+    $obj['attributes'] = serialize($obj['attributes']);
+    $obj['id'] = $obj['hid'];
+    DBUtil::updateObject($obj, 'feproc_handlers');
 
     // Let the calling process know that we have finished successfully
     return true;
@@ -329,17 +158,9 @@ function feproc_handleradminapi_modulehandlers($args)
 {
     extract($args);
 
-    if (!pnModAPILoad('feproc', 'handleradmin')) {
-        pnSessionSetVar('errormsg', _FXMODLOADFAILED);
-        pnRedirect(pnModURL('feproc', 'admin', 'view'));
-        return true;
-    }
-
     // Get the list of APIs for the module.
     // TODO: handle there being no APIs in the module.
-    $apilist = pnModAPIFunc('feproc',
-            'handleradmin',
-            'moduleapilist',
+    $apilist = pnModAPIFunc('feproc', 'handleradmin', 'moduleapilist',
             array('modulename' => $modulename));
 
     if (!$apilist)
